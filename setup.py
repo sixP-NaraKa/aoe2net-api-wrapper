e9b198e6026fd5ab10bc4e2ec6699fa861bc0cb8
sixP-NaraKa <71403526+sixP-NaraKa@users.noreply.github.com>
2020-11-07 14:34:21 +0100

    read README.md, add email

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
 
+readme = ""
+with open("README.md") as f:
+    readme = f.read()
+
 setup(
-    name="aoe2netapi",
+    name="aoe2netapi-wrapper",
     version="0.1.0",
-    description="A simple and basic API wrapper for the https://aoe2.net/#api.",
+    description="A simple and basic Python wrapper for the https://aoe2.net/#api API.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     url="ENTER_GITHUB_LINK_HERE",
     license="MIT",
     author="sixP-NaraKa",
-    author_email="ADD EMAIL HERE",
+    author_email="sixpaths-naraka@protonmail.com",
     packages=find_packages(),
     install_requires=[
         "requests>=2.20.0"
@@ -15,6 +21,6 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python 3",
+        "Programming Language :: Python :: 3",
     ]
 )
